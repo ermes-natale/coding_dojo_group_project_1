a04dbeecca6475c6eba3312d638b6690c5e363a5
QLathe <74535409+QLathe@users.noreply.github.com>
2021-01-27 18:54:09 -0800

    Update views.py

--- crow_app/views.py
+++ crow_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render, redirect
 
+def index(request):
+    if 'username' in request.session:
+        context ={
+            'user': request.session['username'],
+        }
+        return render(request, 'success.html', context)
+    return redirect("/login")
 # Create your views here.
