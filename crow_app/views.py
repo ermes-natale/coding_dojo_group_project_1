838512adb5c6df152751dddb6a25fabcfee08b95
QLathe <74535409+QLathe@users.noreply.github.com>
2021-01-28 15:46:18 -0800

    Update views.py

--- crow_app/views.py
+++ crow_app/views.py
@@ -4,7 +4,7 @@ from login_and_registration_app.models import User
 def index(request):
     if 'username' in request.session:
         context ={
-            'user': request.session['username'],
+            'user': User.objects.get(id=request.session['userid']),
         }
         return render(request, 'success.html', context)
     return redirect("/login")
