030bf4b6670f7f75456c05fbb8938c941cbc1be7
ermes-natale <ermes.natale@gmail.com>
2021-01-28 13:47:53 -0700

    initial update

--- crow_app/models.py
+++ crow_app/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 from login_and_registration_app.models import User
+# import re
+
+class PostManager(models.Manager):
+    def validate_post(self, postData):
+        errors = {}
+        if len(postData['text']) < 1:
+            errors['text'] = "Content required"
 
 # Create your models here.
+class Post(models.Model):
+    text = models.CharField(max_length=144)
+    likes = models.IntegerField()
+    author = models.ForeignKey(User, related_name="posts", on_delete = models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = PostManager()
--- login_and_registration_app/models.py
+++ login_and_registration_app/models.py
@@ -26,4 +26,7 @@ class User(models.Model):
     email = models.CharField(max_length = 255)
     password = models.CharField(max_length = 255)
     objects = UserManager()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 # Create your models here.
